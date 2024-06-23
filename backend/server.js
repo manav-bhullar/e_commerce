b1a62232907a6ba9e3d9470eeaa4bcde43544b64
NEW-BORN <manavbhullar341@gmail.com>
2024-06-23 12:52:05 +0530

    feat: Add error handling for unhandled promise rejections

--- backend/server.js
+++ backend/server.js
@@ -27,3 +27,14 @@ const server = app.listen(process.env.PORT,() =>{
 
 
 
+// unhandled promise rejection
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Shutting down the server for ${err.message}`)
+    console.log(`Shutting down the server for unhandle promise rejection`)
+
+
+    server.close(() => {
+        process.exit(1)
+    })
+})
